30bf47c4bfbc34e1c90bd767ce093057ab51a913
JaeYeopHan <ljyhanll@gmail.com>
2019-12-08 00:18:48 +0900

    fix: Change App.tsx template

--- template/src/components/App.tsx
+++ template/src/components/App.tsx
@@ -3,7 +3,11 @@ import React from "react";
 export default () => {
   return (
     <h1>
-      <a href="" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://github.com/JaeYeopHan/cra-template-unicorn"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         cra-template-unicorn
       </a>
     </h1>
