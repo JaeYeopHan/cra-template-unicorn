74724e2f51ae6717046d6fcc720406b79a6f76cb
JaeYeopHan <ljyhanll@gmail.com>
2020-02-02 22:32:02 +0900

    feat: Add emotion config

--- template/craco.config.js
+++ template/craco.config.js
@@ -1,19 +1,29 @@
-const path = require("path");
-const resolve = arg => path.resolve(__dirname, arg);
+const path = require('path')
+const resolve = arg => path.resolve(__dirname, arg)
 
 module.exports = function() {
   return {
+    babel: {
+      plugins: [
+        [
+          'emotion',
+          {
+            labelFormat: '[filename]--[local]',
+          },
+        ],
+      ],
+    },
     webpack: {
       alias: {
-        "@": resolve("src")
-      }
+        '@': resolve('src'),
+      },
     },
     jest: {
       configure: {
         moduleNameMapper: {
-          "^@/(.*)$": "<rootDir>/src/$1"
-        }
-      }
-    }
-  };
-};
+          '^@/(.*)$': '<rootDir>/src/$1',
+        },
+      },
+    },
+  }
+}
